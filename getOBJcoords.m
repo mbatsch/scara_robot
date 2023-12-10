69b2f8a935887f6196d38e493095f42a7381c801
mbatsch <150702300+mbatsch@users.noreply.github.com>
2023-12-10 23:16:26 +0100

    Create getOBJcoords.m

---
+++ getOBJcoords.m
@@ -0,0 +1,44 @@
+function centroids = getOBJcoords(r)%#codegen
+
+    w = webcam(r,'/dev/video0','640x480');
+    rgbImage = snapshot(w);
+    
+   % gsImage=rgb2gray(rgbImage);
+    [im,jm,km]=size(rgbImage);
+    binaryMask=zeros(im,jm);
+    for i=1:im 
+        for j=1:jm
+            if j>=98 && j<=585 && i>=11 && i<=412
+                binaryMask(i,j)=1;
+            else
+                binaryMask(i,j)=0;
+            end
+        end
+    end
+    
+    kern = [1 2 1; 0 0 0; -1 -2 -1];
+    h = conv2(rgbImage(:,:,1),kern,'same');
+    v = conv2(rgbImage(:,:,1),kern','same');
+    e = sqrt(h.*h + v.*v);
+    BW = logical(((e > 25) * 340 ).*binaryMask);
+    
+    SE = strel("disk",1);
+    
+    BW=imdilate(BW,SE);
+    BW = imfill(BW,'holes');
+    BW=imerode(BW,SE);
+    BW = bwareaopen(BW, 100);
+    
+    hBlob = vision.BlobAnalysis('AreaOutputPort',false,'BoundingBoxOutputPort',false);
+    centroids = hBlob(BW);
+%     
+%     BW = im2uint8(BW);
+%     newImg=cat(3,BW,BW,BW);
+%     
+%     for i=1:10
+%     displayImage(r, newImg,'Title','Dot Detection');
+%     delay(1);
+%     end
+
+
+end
