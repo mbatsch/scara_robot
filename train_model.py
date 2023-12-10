4897dfbf4fd8a63f2bde342d1ff203735de6615c
mbatsch <150702300+mbatsch@users.noreply.github.com>
2023-12-10 23:29:59 +0100

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential, load_model, model_from_json
+from tensorflow.keras.layers import Activation, Dense, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_path = 'data/train'
+valid_path = 'data/valid'
+test_path = 'data/test'
+
+train_batch = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=train_path, target_size=(90, 90), classes=['good','bad'], batch_size=10)
+valid_batch = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=valid_path, target_size=(90, 90), classes=['good','bad'], batch_size=10)
+test_batch = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=test_path, target_size=(90, 90), classes=['good','bad'], batch_size=10, shuffle=False)
+
+imgs, labels = next(train_batch)
+
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20, 20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+plotImages(imgs)
+print(labels)
+
+model = Sequential([
+    Conv2D(filters=32, kernel_size=(3,3), activation='relu', padding='same', input_shape=(90, 90, 3)),
+    MaxPool2D(pool_size=(2,2), strides=2),
+    Conv2D(filters=64, kernel_size=(3,3), activation='relu', padding='same'),
+    MaxPool2D(pool_size=(2,2), strides=2),
+    Flatten(),
+    Dense(units=2, activation='softmax'),
+])
+model.summary()
+
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=train_batch,
+          validation_data=valid_batch,
+          epochs=10,
+          verbose=2
+          )
+
+model.save('model1.h5')
